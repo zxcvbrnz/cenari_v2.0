82ff7ac2bc9c4f77500ee8b4e9cdce35fab28606
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-07-22 13:18:30 +0800

    Add verify_token configuration for WhatsApp service

--- config/services.php
+++ config/services.php
@@ -44,6 +44,6 @@ return [
     'whatsapp' => [
         'token' => env('WHATSAPP_TOKEN'),
         'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
+        'verify_token' => env('WHATSAPP_VERIFY_TOKEN'),
     ],
-
 ];
