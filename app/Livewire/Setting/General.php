c0216b37c9fbbd1201c6985186e2b3624b9f44bb
zxcvbrnz <muhammadsabirin2004@gmail.com>
2026-07-22 14:34:16 +0800

    Add WhatsApp settings management to General component and update UI labels for clarity

--- app/Livewire/Setting/General.php
+++ app/Livewire/Setting/General.php
@@ -10,17 +10,21 @@ class General extends Component
     public $settings;
     public $setting1;
     public $setting2;
+    public $whatsappSetting;
     public bool $setting1edit; // Mengubah ke boolean untuk mencerminkan status checkbox
     public bool $setting2edit; // Mengubah ke boolean untuk mencerminkan status checkbox
+    public bool $whatsappSettingEdit; // Mengubah ke boolean untuk mencerminkan status checkbox
 
     public function mount()
     {
         $this->settings = Setting::all();
         $this->setting1 = Setting::findOrFail(1);
         $this->setting2 = Setting::findOrFail(2);
+        $this->whatsappSetting = Setting::findOrFail(3);
         // Mengatur nilai awal checkbox berdasarkan nilai dari database
         $this->setting1edit = $this->setting1->value === 'ON';
         $this->setting2edit = $this->setting2->value === 'ON';
+        $this->whatsappSettingEdit = $this->whatsappSetting->value === 'ON';
     }
 
     public function update()
@@ -28,9 +32,11 @@ class General extends Component
         // Simpan nilai checkbox ke database
         $this->setting1->value = $this->setting1edit ? 'ON' : 'OFF';
         $this->setting2->value = $this->setting2edit ? 'ON' : 'OFF';
+        $this->whatsappSetting->value = $this->whatsappSettingEdit ? 'ON' : 'OFF';
 
         $this->setting1->save();
         $this->setting2->save();
+        $this->whatsappSetting->save();
 
         $this->dispatch('alert-success', message: 'Berhasil Mengupdate Pengaturan');
     }
--- resources/views/livewire/setting/general.blade.php
+++ resources/views/livewire/setting/general.blade.php
@@ -5,7 +5,7 @@
     <hr>
     <div class="p-6">
         <form wire:submit.prevent="update"> <!-- .prevent untuk mencegah refresh -->
-            <div class="text-slate-700 mb-5">Instruktur</div>
+            <div class="text-slate-700 mb-5">Pengaturan Instruktur</div>
             <div class="mb-8 px-2">
                 <div class="flex justify-between items-center">
                     <div class="text-sm md:text-md w-4/5 text-slate-700">{{ $setting1->key }}
@@ -20,7 +20,7 @@
                 </div>
                 <span class="text-xs text-slate-400">{{ __('(Minimal 10 dan 12 kali Absen)') }}</span>
             </div>
-            <div class="text-slate-700 mb-5">Peserta</div>
+            <div class="text-slate-700 mb-5">Pengaturan Peserta</div>
             <div class="mb-8 px-2">
                 <div class="flex justify-between items-center">
                     <div class="text-sm md:text-md w-4/5 text-slate-700">{{ $setting2->key }}
@@ -35,8 +35,23 @@
                 </div>
                 {{-- <span class="text-xs text-slate-400">{{ __('(Minimal 10 dan 12 kali Absen)') }}</span> --}}
             </div>
+            <div class="text-slate-700 mb-5">Pengaturan Admin</div>
+            <div class="mb-8 px-2">
+                <div class="flex justify-between items-center">
+                    <div class="text-sm md:text-md w-4/5 text-slate-700">{{ $whatsappSetting->key }}
+                    </div>
+                    <label
+                        class="relative inline-block h-6 w-12 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-teal-600">
+                        <input class="peer sr-only" id="AcceptConditions" type="checkbox" wire:model="whatsappSettingEdit" />
+                        <!-- Binding langsung ke $whatsappSettingEdit -->
+                        <span
+                            class="absolute inset-y-0 start-0 m-1 size-4 rounded-full bg-gray-300 ring-[6px] ring-inset ring-white transition-all peer-checked:start-6 peer-checked:bg-white peer-checked:ring-transparent"></span>
+                    </label>
+                </div>
+                {{-- <span class="text-xs text-slate-400">{{ __('(Minimal 10 dan 12 kali Absen)') }}</span> --}}
+            </div>
             <div class="pt-4">
-                <x-primary-button>{{ __('Simpan') }}</x-primary-button>
+                <x-primary-button>{{ __('Simpan Perubahan') }}</x-primary-button>
             </div>
         </form>
     </div>
